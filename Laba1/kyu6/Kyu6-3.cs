0bda9652fc9a23da7135195c7de9868f242bb5d3
6988897 <6988897@gmail.com>
2024-11-15 01:15:36 +0300

    Add files via upload

---
+++ Laba1/kyu6/Kyu6-3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProg
+{
+    internal class Kyu6_3
+    {
+        public static int DigitalRoot(long n)
+        {
+            if (n < 10)
+            {
+                return Convert.ToInt32(n);
+            }
+
+            int sum = 0;
+            while (n > 0)
+            {
+                sum += Convert.ToInt32(n % 10);
+                n /= 10;
+            }
+
+            return DigitalRoot(sum);
+        }
+    }
+}
