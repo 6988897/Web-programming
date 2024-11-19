0881d7d75e4710271dc3cc63f774d067fec0c53f
6988897 <6988897@gmail.com>
2024-11-19 18:27:23 +0300

    Add files via upload

---
+++ Laba1/kyu6/Kyu6-2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProg
+{
+    internal class Kyu6_2
+    {
+        //Учитывая строку, состоящую только из буквенных символов и не содержащую пробелов,
+        //верните наибольшее значение подстрок, состоящих из согласных.
+        //Например, в слове «зодиак» зачеркнём гласные. Получится: «z o d ia c». "zodiac" -> 26
+        //Подстроки с согласными: "z", "d" и "c" со значениями "z" = 26, "d" = 4 и "c = 3.
+        //Наибольшее значение — 26.
+        public static int Solve(string s)
+        {
+            Dictionary<char, int> alphabet = new Dictionary<char, int>();
+            for (int i = 0; i < 26; i++)
+            {
+                alphabet[(char)('a' + i)] = i + 1;
+            }
+
+            int sum = 0;
+            int max = 0;
+
+            foreach (char c in s)
+            {
+                if (!"aeiou".Contains(c))
+                {
+                    sum += alphabet[c];
+                    if (sum > max)
+                    {
+                        max = sum;
+                    }
+                }
+                else
+                {
+                  sum = 0;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ Laba1/kyu6/Kyu6-3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProg
+{
+    internal class Kyu6_3
+    {
+        //Учитывая n, найдите сумму цифр n. Если в этом значении больше одной цифры,
+        //продолжайте уменьшать его таким образом, пока не получится однозначное число.
+        //На вход подаётся неотрицательное целое число.
+        public static int DigitalRoot(long n)
+        {
+            if (n < 10)
+            {
+                return Convert.ToInt32(n);
+            }
+
+            int sum = 0;
+            while (n > 0)
+            {
+                sum += Convert.ToInt32(n % 10);
+                n /= 10;
+            }
+
+            return DigitalRoot(sum);
+        }
+    }
+}
