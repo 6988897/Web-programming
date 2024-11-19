707dcd5ebfedbea53e67c5679214857665a3f2f6
6988897 <6988897@gmail.com>
2024-11-19 18:44:34 +0300

    Add files via upload

---
+++ Laba1/kyu5/kyu5-2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CaesarCipher
+{
+    //Метод шифрует строку s с использованием шифра Цезаря.  
+    public static List<string> movingShift(string s, int shift)
+    {
+        char[] result = new char[s.Length];
+        for (int i = 0; i < s.Length; i++)
+        {
+            result[i] = ShiftChar(s[i], shift + i);
+        }
+        string encoded = new string(result);
+
+        int partLength = (encoded.Length + 4) / 5;
+        List<string> parts = new List<string>();
+        for (int i = 0; i < 5; i++)
+        {
+            int start = i * partLength;
+            int length = Math.Min(partLength, encoded.Length - start);
+            parts.Add(encoded.Substring(start, length));
+        }
+        return parts;
+    }
+    public static string demovingShift(List<string> s, int shift)
+    {
+        
+        string combined = string.Join("", s);
+
+        char[] result = new char[combined.Length];
+        for (int i = 0; i < combined.Length; i++)
+        {
+            result[i] = ShiftChar(combined[i], -shift - i);
+        }
+        return new string(result);
+    }
+    private static char ShiftChar(char c, int shift)
+    {
+        if (char.IsLetter(c))
+        {
+            char offset = char.IsUpper(c) ? 'A' : 'a';
+            return (char)(((c - offset + shift) % 26 + 26) % 26 + offset);
+        }
+        return c;
+    }
+}
---
+++ Laba1/kyu5/kyu5-3.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProg
+{
+    internal class kyu5_3
+    {
+        //Вес строки равен сумме весов всех её цифр.
+        //Вам нужно отсортировать строки в порядке возрастания веса.
+       public static string orderWeight(string str)
+        {
+            
+            string[] numbers = str.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int weightA = 0;
+                    foreach (char c in numbers[i])
+                    {
+                        weightA += c - '0';
+                     }
+                    int weightB = 0;
+
+                    foreach (char c in numbers[j])
+                    {
+                        weightB += c - '0';
+                    }
+
+                    if (weightA > weightB || (weightA == weightB && string.Compare(numbers[i], numbers[j], StringComparison.Ordinal) > 0))
+                    {
+                        
+                        string temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+
+            return string.Join(" ", numbers);
+        }
+
+     
+    }
+}
